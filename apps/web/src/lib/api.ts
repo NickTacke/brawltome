5554efe25d00dbb51ea5da44910df3517c4a298d
NickTacke <the.nick.tacke@gmail.com>
2025-12-06 22:37:29 +0100

    Enable CORS in API and update default port to 8080; add fetcher function in new api.ts file for data fetching with error handling.

---
+++ apps/web/src/lib/api.ts
@@ -0,0 +1,10 @@
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080';
+
+export async function fetcher(path: string) {
+    const response = await fetch(`${API_URL}${path}`, {cache: 'no-store'});
+    if (!response.ok) {
+        if (response.status === 404) return null; // Gracefully handle 404s
+        throw new Error('Failed to fetch data', { cause: response.statusText });
+    }
+    return response.json();
+}
