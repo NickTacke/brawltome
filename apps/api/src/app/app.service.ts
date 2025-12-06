86ed6f81561dfe46b737203a70488cbac95ab338
NickTacke <the.nick.tacke@gmail.com>
2025-12-06 01:12:33 +0100

    Update dependencies and enhance API functionality: add @prisma/adapter-pg and pg to package.json, implement PrismaService for database connection, and introduce getPlayer method in AppService to fetch player data by brawlhallaId.

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,8 +1,17 @@
+import { Player } from '@brawltome/database';
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
 
 @Injectable()
 export class AppService {
+  constructor(private prisma: PrismaService) {}
+
   getData(): { message: string } {
     return { message: 'Hello API' };
+  }
+
+  async getPlayer(brawlhallaId: number): Promise<Player> {
+    return await this.prisma.player.findMany({
+    }) ?? null;
   }
 }
