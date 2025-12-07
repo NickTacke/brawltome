ae6a8489486b48a63e5a91afadc81c5087cbf1f7
NickTacke <the.nick.tacke@gmail.com>
2025-12-07 22:45:12 +0100

    Add clan interfaces: introduce ClanMemberDTO and ClanDTO to define clan structure and member details for improved type safety and data management.

---
+++ libs/shared-types/src/lib/clan.interface.ts
@@ -0,0 +1,17 @@
+export interface ClanMemberDTO {
+    brawlhalla_id: number;
+    name: string;
+    rank: string;
+    join_date: number;
+    xp: number;
+}
+
+export interface ClanDTO {
+    clan_id: number;
+    clan_name: string;
+    clan_create_date: number;
+    clan_xp: string;
+    clan_lifetime_xp: number;
+    clan: ClanMemberDTO[];
+}
+
