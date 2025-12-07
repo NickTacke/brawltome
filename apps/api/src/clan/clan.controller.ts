998e586853880240aa1311d826066cec7e3e1161
NickTacke <the.nick.tacke@gmail.com>
2025-12-07 22:43:33 +0100

    Add Clan module: implement ClanController and ClanService for clan data management

---
+++ apps/api/src/clan/clan.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { ClanService } from './clan.service';
+
+@Controller('clan')
+export class ClanController {
+    constructor(private readonly clanService: ClanService) {}
+
+    @Get(':id')
+    getClan(@Param('id', ParseIntPipe) id: number) {
+        return this.clanService.getClan(id);
+    }
+}
+
