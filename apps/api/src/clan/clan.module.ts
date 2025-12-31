7fd1d41e6c9a4aac80d7885fa8cfef6c87573878
NickTacke <the.nick.tacke@gmail.com>
2025-12-31 00:13:37 +0000

    feat: integrate queue system for clan data refresh and enhance discovery logic in ClanService

--- apps/api/src/clan/clan.module.ts
+++ apps/api/src/clan/clan.module.ts
@@ -3,9 +3,10 @@ import { ClanController } from './clan.controller';
 import { ClanService } from './clan.service';
 import { DatabaseModule } from '@brawltome/database';
 import { BhApiClientModule } from '@brawltome/bhapi-client';
+import { QueueModule } from '../queue/queue.module';
 
 @Module({
-  imports: [DatabaseModule, BhApiClientModule],
+  imports: [DatabaseModule, BhApiClientModule, QueueModule],
   controllers: [ClanController],
   providers: [ClanService],
 })
