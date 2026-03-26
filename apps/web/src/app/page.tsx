a7ac16ed915fcc4caf0dc770ca29a8034f9d52fe
NickTacke <the.nick.tacke@gmail.com>
2026-03-27 00:01:10 +0100

    feat: add X (Twitter) social link to home page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -28,6 +28,18 @@ export default function Home() {
           </div>
         </Link>
         <Link
+          href="https://x.com/brawltome"
+          target="_blank"
+          className="group transition-transform hover:scale-105"
+        >
+          <div className="flex items-center justify-center w-10 h-10 bg-black rounded-full transition-opacity hover:opacity-90">
+            <svg width="18" height="18" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">
+              <title>X</title>
+              <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
+            </svg>
+          </div>
+        </Link>
+        <Link
           href="https://github.com/NickTacke/brawltome"
           target="_blank"
           className="group transition-transform hover:scale-105"
