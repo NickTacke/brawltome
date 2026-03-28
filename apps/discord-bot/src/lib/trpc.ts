32e84399ffb7347463184b0a498b5a98f48f496c
NickTacke <the.nick.tacke@gmail.com>
2026-03-28 20:15:05 +0100

    feat(discord-bot): add internal secret header to tRPC client

--- apps/discord-bot/src/lib/trpc.ts
+++ apps/discord-bot/src/lib/trpc.ts
@@ -3,12 +3,16 @@ import { createTRPCClient, httpBatchLink } from '@trpc/client'
 import superjson from 'superjson'
 
 const apiUrl = process.env.API_URL ?? 'http://localhost:3000'
+const internalSecret = process.env.INTERNAL_API_SECRET ?? ''
 
 export const api = createTRPCClient<AppRouter>({
   links: [
     httpBatchLink({
       url: `${apiUrl}/trpc`,
       transformer: superjson,
+      headers: {
+        ...(internalSecret ? { 'x-internal-secret': internalSecret } : {}),
+      },
     }),
   ],
 })
