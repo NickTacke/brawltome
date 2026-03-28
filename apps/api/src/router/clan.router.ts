1165c1d13612eda9baf5023a9c7a1000b61a5361
NickTacke <the.nick.tacke@gmail.com>
2026-03-28 20:14:25 +0100

    feat(api): protect player and clan endpoints with internalProcedure

--- apps/api/src/router/clan.router.ts
+++ apps/api/src/router/clan.router.ts
@@ -1,9 +1,9 @@
 import { z } from 'zod'
 import { getClan } from '../services/clan.service'
-import { publicProcedure, router } from '../trpc/trpc'
+import { internalProcedure, router } from '../trpc/trpc'
 
 export const clanRouter = router({
-  byId: publicProcedure.input(z.object({ id: z.number().int().positive() })).query(async ({ ctx, input }) => {
+  byId: internalProcedure.input(z.object({ id: z.number().int().positive() })).query(async ({ ctx, input }) => {
     return getClan(ctx, input.id)
   }),
 })
